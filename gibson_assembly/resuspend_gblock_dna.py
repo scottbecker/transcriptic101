0c14670bc55ea84d2b5434044c4e8082cf396466
Scott Becker <scottjbecker@gmail.com>
2016-04-20 20:15:13 +0200

    finished resuspend protocol

--- gibson_assembly/resuspend_gblock_dna.py
+++ gibson_assembly/resuspend_gblock_dna.py
@@ -1,29 +1,29 @@
 import json
 from autoprotocol.protocol import Protocol
-
+from utils import ul
 
 inv = {
-#    'gblock_dna':'ct18vs7cmjat2c'   #do not use until we want to do this for real
-     'gblock_dna':''    #
-
+    'gblock_dna':'ct18vs7cmjat2c',     # my inventory
+     'te': 'rs17pwyc754v9t',           # catalog; TE
 }
 
 p = Protocol()
 
+#existing inventory
+dna_sample = p.ref("gblock_dna", id=inv['gblock_dna'], cont_type="micro-1.5", storage='cold_20')
 
+#tubes are thawed before being placed into workcell
 
-#ct18vs7cmjat2c
+#Centrifuge the tube for 3-5 sec at a minimum of 3000 x g to pellet the material to the bottom of the tube.
+p.spin(dna_sample, '3000:g', '5:second')
 
-dna_sample = p.ref("bacteria", None, "micro-1.5", discard=True)
-test_plate = p.ref("test_plate", None, "96-flat", storage="cold_4")
+#Add 500uL TE (to reach 1 ng/uL)
+p.provision(inv["te"], dna_sample.well(0), ul(500))
 
-p.dispense_full_plate(test_plate, "lb-broth-noAB", "50:microliter")
-w = 0
-amt = 1
+#Briefly vortex (here we have to use mix) and centrifuge
 
-while amt < 20:
-    p.transfer(bacterial_sample.well(0), test_plate.well(w), "%d:microliter" % amt)
-    amt += 2
-    w +=1
+p.mix(dna_sample.well(0),'480:microliter',speed='480:microliter/second',repetitions=10)
+
+p.spin(dna_sample, '3000:g', '3:second')
 
 print(json.dumps(p.as_dict(), indent=2))
