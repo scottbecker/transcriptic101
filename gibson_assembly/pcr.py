4ece8c3d6bb86c2790f5c9de967efb44d533ee8d
Scott Becker <scottjbecker@gmail.com>
2016-04-26 21:55:04 +0200

    added custom protocol that I later found out wasn't needed

--- gibson_assembly/pcr.py
+++ gibson_assembly/pcr.py
@@ -3,20 +3,22 @@
 Derived from Brian Naughton @ http://blog.booleanbiotech.com/genetic_engineering_pipeline_python.html
 
 """
-
-from utils import ul
+import sys
+import json
+from autoprotocol.protocol import Protocol
+from utils import (ul, expid, init_inventory_well, touchdown,
+                   dead_volume)
+import numpy
 
 p = Protocol()
 
 # ---------------------------------------------------
 # Set up experiment
 #
-experiment_name = "sfgfp_pcroe_v8"
-template_length = 740
+experiment_name = "sfgfp_pcroe_v9"
+template_length = 726
 
-_options = {'dilute_template': False, # if working stock has not been made
-            'dilute_dNTP'    : False, # if working stock has not been made
-            'run_gel'        : True,  # run a gel to see the plasmid size
+_options = {'run_gel'        : True,  # run a gel to see the plasmid size
             'run_absorbance' : False, # check absorbance at 260/280/320
             'run_sanger'     : False} # sanger sequence the new sequence
 options = {k for k,v in _options.items() if v is True}
@@ -36,17 +38,18 @@ options = {k for k,v in _options.items() if v is True}
 #@ToDo: confirm my 1ng/uL is correct
 
 inv = {
-    'Q5 Polymerase':                     'rs16pcce8rdytv', # catalog; Q5 High-Fidelity DNA Polymerase
-    'Q5 Buffer':                         'rs16pcce8rmke3', # catalog; Q5 Reaction Buffer
-    'dNTP Mixture':                      'rs16pcb542c5rd', # catalog; dNTP Mixture (25mM?)
+    'pcr_reagent_plate':                 'ct18x95j499zx4', # inventory: A1: Q5 polymerase,
+                                                           # A2: Buffer, A3: Enhancer, B1: dNTP 10mM
     'water':                             'rs17gmh5wafm5p', # catalog; Autoclaved MilliQ H2O
-    'sfgfp_puc19_primer_forward_10uM':  'ct18wxfe2dxmmd', # inventory; micro-1.5, cold_4, 10uM
-    'sfgfp_puc19_primer_reverse_10uM':  'ct18wxfdgqfyjp', # inventory; micro-1.5, cold_4, 10uM
-    'sfgfp_2nM':                       'ct18vs7cmjat2c', # inventory; sfGFP tube #1, micro-1.5, cold_20, 2.12nM, 1ng/uL
+    'sfgfp_puc19_primer_forward_10uM':   'ct18wxfe2dxmmd', # inventory; micro-1.5, cold_4, 10uM
+    'sfgfp_puc19_primer_reverse_10uM':   'ct18wxfdgqfyjp', # inventory; micro-1.5, cold_4, 10uM
+    'sfgfp_2nM':                         'ct18vs7cmjat2c', # inventory; sfGFP tube #1, micro-1.5, cold_20, 2.12nM, 1ng/uL
 }
 
 if "--test" in sys.argv:
     test_inv = {
+        'pcr_reagent_plate':                'ct18x92yfcbhhz', # inventory: A1: Q5 polymerase,
+                                                              # A2: Buffer, A3: Enhancer, B1: dNTP 10mM
         'sfgfp_puc19_primer_forward_10uM':  'ct18x626u9nvne', # inventory; micro-1.5, cold_4, 10uM
         'sfgfp_puc19_primer_reverse_10uM':  'ct18x626u9yshq', # inventory; micro-1.5, cold_4, 10uM
         'sfgfp_2nM':                        'ct18x62qg8km37', # inventory
@@ -61,14 +64,19 @@ primer_wells = [p.ref('sfgfp_puc19_primer_forward_10uM', id=inv['sfgfp_puc19_pri
                 p.ref('sfgfp_puc19_primer_reverse_10uM', id=inv['sfgfp_puc19_primer_reverse_10uM'], 
                       cont_type="micro-1.5", storage="cold_20").well(0)]
 
+pcr_reagent_plate = p.ref("pcr_reagent_plate", id=inv['pcr_reagent_plate'], 
+                          cont_type="micro-1.5", storage="cold_20")
+
+q5_poly_well = pcr_reagent_plate.wells(["A1"])[0]
+q5_buffer_well = pcr_reagent_plate.wells(["A2"])[0]
+dNTP_well = pcr_reagent_plate.wells(["B1"])[0]
+
 
 # New inventory resulting from this experiment
-dilute_template_tube = p.ref("sfgfp1_0.25ngul",  cont_type="micro-1.5", storage="cold_4").well(0)
-dNTP_10uM_tube       = p.ref("dNTP_10uM",        cont_type="micro-1.5", storage="cold_4").well(0)
-sfgfp_pcroe_out_tube = p.ref(expid("amplified"), cont_type="micro-1.5", storage="cold_4").well(0)
+sfgfp_pcroe_out_tube = p.ref(expid("amplified",experiment_name), cont_type="micro-1.5", storage="cold_4").well(0)
 
 # Temporary tubes for use, then discarded
-mastermix_tube = p.ref("mastermix", cont_type="micro-1.5", storage="cold_4",  discard=True).well(0)
+mastermix_well = p.ref("mastermix", cont_type="micro-1.5", storage="cold_4",  discard=True).well(0)
 water_tube =     p.ref("water",     cont_type="micro-1.5", storage="ambient", discard=True).well(0)
 pcr_plate =      p.ref("pcr_plate", cont_type="96-pcr",    storage="cold_4",  discard=True)
 if 'run_absorbance' in options:
@@ -78,59 +86,44 @@ if 'run_absorbance' in options:
 all_inventory_wells = [template_tube] + primer_wells
 for well in all_inventory_wells:
     init_inventory_well(well)
-    print(well.name, well.volume, well.properties)
-
 
 # -----------------------------------------------------
 # Provision water once, for general use
 #
 p.provision(inv["water"], water_tube, ul(500))
 
-
-# -----------------------------------------------------
-# Dilute template 1/10 (10ng/ul->1ng/ul) and keep at 4C
-# OR
-# Dilute template 1/40 (10ng/ul->0.25ng/ul) and keep at 4C
-#
-if 'dilute_template' in options:
-    p.transfer(water_tube, dilute_template_tube, ul(195))
-    p.mix(dilute_template_tube, volume=ul(100), repetitions=10)
-
-# Dilute dNTP to exactly 10uM
-if 'dilute_DNTP' in options:
-    p.transfer(water_tube,           dNTP_10uM_tube, ul(6))
-    p.provision(inv["dNTP Mixture"], dNTP_10uM_tube, ul(4))
-
-
 # -----------------------------------------------------
 # Q5 PCR protocol
 # www.neb.com/protocols/2013/12/13/pcr-using-q5-high-fidelity-dna-polymerase-m0491
 #
-# 25ul reaction
-# -------------
-# Q5 reaction buffer      5    ul
-# Q5 polymerase           0.25 ul
-# 10mM dNTP               0.5  ul -- 1ul = 4x12.5mM
-# 10uM primer 1           1.25 ul
-# 10uM primer 2           1.25 ul
-# 1pg-1ng Template        1    ul -- 1ng/ul concentration
+# 25ul reaction (we will run it 4 times, totally 100uL of solution)
+# -------------                      4rxn total
+# Q5 reaction buffer      5    ul --> 20uL
+# Q5 polymerase           0.25 ul --> 1uL
+# 10mM dNTP               0.5  ul --> 2uL
+# 10uM forward primer     1.25 ul --> 5uL
+# 10uM reverse primer     1.25 ul --> 5uL
+# 1ng Template (1 ng/ul) 1 ul --> 4uL (1ng/ul concentration) 
+# water                   
 # -------------------------------
-# Sum                     9.25 ul
+# Sum                     9.25 ul --> 37uL
+# water                   15.75 uL --> 63uL
 #
 #
 
 # Mastermix tube will have 96ul of stuff, leaving space for 4x1ul aliquots of template
-p.transfer(water_tube,             mastermix_tube, ul(64))
-p.provision(inv["Q5 Buffer"],      mastermix_tube, ul(20))
-p.provision(inv['Q5 Polymerase'],  mastermix_tube, ul(1))
-p.transfer(dNTP_10uM_tube,         mastermix_tube, ul(1), mix_before=True, mix_vol=ul(2))
-p.transfer(primer_wells[0], mastermix_tube, ul(5), mix_before=True, mix_vol=ul(10))
-p.transfer(primer_wells[1], mastermix_tube, ul(5), mix_before=True, mix_vol=ul(10))
-p.mix(mastermix_tube, volume="48:microliter", repetitions=10)
+p.transfer(water_tube,                mastermix_well, ul(63))
+p.transfer(q5_buffer_well, mastermix_well, ul(20), mix_before=True, mix_vol=ul(20),mix_after=True)
+p.transfer(q5_poly_well, mastermix_well, ul(1), mix_before=True, mix_vol=ul(5),mix_after=True)
+p.transfer(dNTP_well, mastermix_well, ul(2), mix_before=True, mix_vol=ul(5),mix_after=True)
+p.transfer(primer_wells[0], mastermix_well, ul(5), mix_before=True, mix_vol=ul(10), mix_after=True)
+p.transfer(primer_wells[1], mastermix_well, ul(5), mix_before=True, mix_vol=ul(10), mix_after=True)
+p.mix(mastermix_well, volume="48:microliter", repetitions=10)
 
 # Transfer mastermix to pcr_plate without template
-p.transfer(mastermix_tube, pcr_plate.wells(["A1","B1","C1"]), ul(24))
-p.transfer(mastermix_tube, pcr_plate.wells(["A2"]),           ul(24)) # acknowledged dead volume problems
+p.transfer(mastermix_well, pcr_plate.wells(["A1","B1","C1"]), ul(24))
+#
+p.transfer(mastermix_well, pcr_plate.wells(["A2"]),           ul(24)) # acknowledged dead volume problems
 p.mix(pcr_plate.wells(["A1","B1","C1","A2"]), volume=ul(12), repetitions=10)
 
 # Finally add template
@@ -142,7 +135,7 @@ p.mix(pcr_plate.wells(["A1","B1","C1"]), volume=ul(12.5), repetitions=10)
 # 61.1 annealing temperature is recommended by NEB protocol
 # p.seal is enforced by transcriptic
 #
-extension_time = int(max(2, np.ceil(template_length * (11.0/1000))))
+extension_time = int(max(2, numpy.ceil(template_length * (11.0/1000))))
 assert 0 < extension_time < 60, "extension time should be reasonable for PCR"
 
 cycles = [{"cycles":  1, "steps": [{"temperature": "98:celsius", "duration": "30:second"}]}] + \
@@ -165,7 +158,7 @@ if 'run_gel' in options:
     p.transfer(water_tube, pcr_plate.wells(["D1","E1","F1","D2"]),
                [ul(18),ul(16),ul(12),ul(12)], mix_after=True, mix_vol=ul(10))
     p.gel_separate(pcr_plate.wells(["D1","E1","F1","D2"]),
-                   ul(20), "agarose(10,2%)", "ladder1", "10:minute", expid("gel"))
+                   ul(20), "agarose(10,2%)", "ladder1", "10:minute", expid("gel", experiment_name))
 
 #---------------------------------------------------------
 # Absorbance dilution series. Take 1ul out of the 25ul pcr plate wells
@@ -184,35 +177,31 @@ if 'run_absorbance' in options:
         p.absorbance(abs_plate, abs_plate.wells(abs_wells),
                      "{}:nanometer".format(wavelength), exp_id("abs_{}".format(wavelength)), flashes=25)
 
-# -----------------------------------------------------------------------------
-# Sanger sequencing: https://developers.transcriptic.com/docs/sanger-sequencing
-# "Each reaction should have a total volume of 15 ul and we recommend the following composition of DNA and primer:
-#  PCR product (40 ng), primer (1 ul of a 10 µM stock)"
-#
-#  By comparing to the gel ladder concentration (175ng/lane), it looks like 5ul of PCR product has approximately 30ng of DNA
-#
-if 'run_sanger' in options:
-    p.unseal(pcr_plate)
-    seq_wells = ["G1","G2"]
-    for primer_num, seq_well in [(0, seq_wells[0]),(1, seq_wells[1])]:
-        p.transfer(dilute_primer_tubes[primer_num], pcr_plate.wells([seq_well]),
-                   ul(1), mix_before=True, mix_vol=ul(50))
-        p.transfer(pcr_plate.wells(["A1"]), pcr_plate.wells([seq_well]),
-                   ul(5), mix_before=True, mix_vol=ul(10))
-        p.transfer(water_tube, pcr_plate.wells([seq_well]), ul(9))
+## -----------------------------------------------------------------------------
+## Sanger sequencing: https://developers.transcriptic.com/docs/sanger-sequencing
+## "Each reaction should have a total volume of 15 ul and we recommend the following composition of DNA and primer:
+##  PCR product (40 ng), primer (1 ul of a 10 uM stock)"
+##
+##  By comparing to the gel ladder concentration (175ng/lane), it looks like 5ul of PCR product has approximately 30ng of DNA
+##
+#if 'run_sanger' in options:
+    #p.unseal(pcr_plate)
+    #seq_wells = ["G1","G2"]
+    #for primer_num, seq_well in [(0, seq_wells[0]),(1, seq_wells[1])]:
+        #p.transfer(dilute_primer_tubes[primer_num], pcr_plate.wells([seq_well]),
+                   #ul(1), mix_before=True, mix_vol=ul(50))
+        #p.transfer(pcr_plate.wells(["A1"]), pcr_plate.wells([seq_well]),
+                   #ul(5), mix_before=True, mix_vol=ul(10))
+        #p.transfer(water_tube, pcr_plate.wells([seq_well]), ul(9))
 
-    p.mix(pcr_plate.wells(seq_wells), volume=ul(7.5), repetitions=10)
-    p.sangerseq(pcr_plate, pcr_plate.wells(seq_wells[0]).indices(), expid("seq1"))
-    p.sangerseq(pcr_plate, pcr_plate.wells(seq_wells[1]).indices(), expid("seq2"))
+    #p.mix(pcr_plate.wells(seq_wells), volume=ul(7.5), repetitions=10)
+    #p.sangerseq(pcr_plate, pcr_plate.wells(seq_wells[0]).indices(), expid("seq1",experiment_name))
+    #p.sangerseq(pcr_plate, pcr_plate.wells(seq_wells[1]).indices(), expid("seq2",experiment_name))
 
 # -------------------------------------------------------------------------
 # Then consolidate to one tube. Leave at least 3ul dead volume in each tube
 #
 remaining_volumes = [well.volume - dead_volume['96-pcr'] for well in pcr_plate.wells(["A1","B1","C1"])]
-print("Consolidated volume", sum(remaining_volumes, ul(0)))
 p.consolidate(pcr_plate.wells(["A1","B1","C1"]), sfgfp_pcroe_out_tube, remaining_volumes, allow_carryover=True)
 
-uprint("\nProtocol 1. Amplify the insert (oligos previously synthesized)")
-jprotocol = json.dumps(p.as_dict(), indent=2)
-!echo '{jprotocol}' | transcriptic analyze
-open("protocol_{}.json".format(experiment_name),'w').write(jprotocol)
+print(json.dumps(p.as_dict(), indent=2))
